88fd9a8a1fa67ec15e1b46b4a0d7df2a9297f794
Alex Marzullo <marzullo94@gmail.com>
2021-01-14 15:59:55 -0500

    finished index.js, might need changes later

--- index.js
+++ index.js
@@ -45,4 +45,95 @@ const questions = [
             }
             return true;
         }
-    }],
+    },
+
+    // Project Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project.',
+       
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a description for your project.");
+            }
+            return true;
+        }
+    },
+    // Project Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Explain how user would install (if necessary) for Installation Section.',
+        
+    },
+    // Usage of project
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter your project instructions and examples of it in use for Usage Section.',
+    },
+    // Select license
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license for your project.',
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+        
+    },
+    // Contributing to project
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Explain how users can contribute to your project (if necessary).',
+       
+    },
+    // Test for project
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide tests for project, and explain how to test (if necessary).',
+       
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Your markdown file has been created.')
+    });
+}
+
+// Reference: https://www.npmjs.com/package/util.promisify
+const writeFileAsync = util.promisify(writeToFile);
+
+// function to initialize program
+async function init() {
+    
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Your responses have been logged. Calling to GitHub...");
+
+        // Referencing API.js
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+
+        // Pass inquirer data and api data to markdown
+        console.log("Generating your markdown")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+
+        // Write markdown
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// function call to initialize program
+init();
